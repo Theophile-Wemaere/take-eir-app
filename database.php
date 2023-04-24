5e20d9864ac795e186c9e204229b61f9cf6221cb
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-04-24 17:58:35 +0200

    added database controller

---
+++ database.php
@@ -0,0 +1,39 @@
+<?php
+class DB {
+  public $error = "";
+  private $pdo = null;
+  private $stmt = null;
+  function __construct () {
+    $this->pdo = new PDO(
+      "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET, 
+      DB_USER, DB_PASSWORD, [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+  }
+
+  function __destruct () {
+    if ($this->stmt!==null) { $this->stmt = null; }
+    if ($this->pdo!==null) { $this->pdo = null; }
+  }
+
+  function select ($sql, $data=null) {
+    $this->stmt = $this->pdo->prepare($sql);
+    $this->stmt->execute($data);
+    return $this->stmt->fetchAll();
+  }
+}
+
+define("DB_HOST", "take-eir.fr");
+define("DB_NAME", "take_eir");
+define("DB_CHARSET", "utf8mb4");
+$creds = file(".creds");                                                                                                                                                                                     
+$creds_line = $creds[0];                                                                                                                                                                                     
+$creds_parts = explode(":", $creds_line);                                                                                                                                                                    
+$username = $creds_parts[0];                                                                                                                                                                                 
+$password = $creds_parts[1]; 
+define("DB_USER", $username);
+define("DB_PASSWORD", $password);
+ 
+$_DB = new DB();
+?>
