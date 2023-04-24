1e213873674991b25722071de5c7f944bc1e41ce
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-04-24 17:24:35 +0200

    cleaned javascript in files

--- JS/scripts.js
+++ JS/scripts.js
@@ -17,3 +17,20 @@ window.onclick = function (e) {
     }
   }
 };
+
+function dropMenuPres() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
