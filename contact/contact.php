7ca6e4b26ace6640fbc33269688e2a3fe1bd7b04
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-04-26 20:02:46 +0200

    fixed responsive on product page

--- contact/contact.php
+++ contact/contact.php
@@ -19,7 +19,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Nunito&display=swap" rel="stylesheet" />
 </head>
 
-
 <body>
     <div class="top-bar">
         <div class="top-bar-img">
@@ -139,20 +138,20 @@
             }
         </script>
     </div>
-</body>
-<div class="bottom-bar">
-    <a>© take-eir</a>
-    <a href="/contact/contact.php">Nous contacter</a>
-    <div class="medias">
-        <!-- https://icons8.com/icons/set/social-media -->
-        <a href="https://linkedin.com/"><img src="/images/icons8-linkedin-24.png" />
-        </a>
-        <a href="https://youtube.com/"><img src="/images/icons8-youtube-logo-24.png" /></a>
-        <a href="https://twitter.com/"><img src="/images/icons8-twitter-24.png" />
-        </a>
-        <a href="https://instagram.com/"><img src="/images/icons8-instagram-24.png" />
-        </a>
+    <div class="bottom-bar">
+        <a>© take-eir</a>
+        <a href="/contact/contact.php">Nous contacter</a>
+        <div class="medias">
+            <!-- https://icons8.com/icons/set/social-media -->
+            <a href="https://linkedin.com/"><img src="/images/icons8-linkedin-24.png" />
+            </a>
+            <a href="https://youtube.com/"><img src="/images/icons8-youtube-logo-24.png" /></a>
+            <a href="https://twitter.com/"><img src="/images/icons8-twitter-24.png" />
+            </a>
+            <a href="https://instagram.com/"><img src="/images/icons8-instagram-24.png" />
+            </a>
+        </div>
     </div>
-</div>
+</body>
 
 </html>
