9456be183d9d5a0b1b369a6a47c826d19b8aae25
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-04-27 00:41:27 +0200

    admin page work well (see users, delete, reset password)

--- admin-controller.php
+++ admin-controller.php
@@ -1,9 +1,35 @@
 <?php
 require "database.php";
 
-$results = $_DB->execute(
-    "SELECT id_user, name, surname, email, created_at, roles.role_name from users join roles on users.id_role = roles.id_role WHERE surname LIKE ?",
-    ["%{$_POST["search"]}%"]
-)->fetchAll();
+if (isset($_POST["action"])) {
+    switch ($_POST["action"]) {
+        case "delete":
+            $_DB->execute("DELETE FROM users WHERE id_user = ?", [$_POST["id_user"]]);
+            break;
 
-echo json_encode(count($results) == 0 ? null : $results);
+        case "reset":
+            // Generate hashed password using PASSWORD_ARGON2ID algorithm
+            $options = [
+                "memory_cost" => PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+                "time_cost" => PASSWORD_ARGON2_DEFAULT_TIME_COST,
+                "threads" => PASSWORD_ARGON2_DEFAULT_THREADS,
+            ];
+            $hashedPassword = password_hash(
+                "take-eir",
+                PASSWORD_ARGON2ID,
+                $options
+            );
+
+            $_DB->execute("UPDATE users SET password = :hash WHERE id_user = :id", [
+                "hash" => $hashedPassword,
+                "id" => $_POST["id_user"]
+            ]);
+            break;
+    }
+} elseif (isset($_POST["search"])) {
+    $_DB->execute("SET @search = :var", ["var" => "%{$_POST["search"]}%"]);
+    $results = $_DB->execute(
+        "SELECT id_user, name, surname, email, created_at, roles.role_name from users join roles on users.id_role = roles.id_role WHERE name LIKE @search OR surname LIKE @search OR email LIKE @search"
+    )->fetchAll();
+    echo json_encode(count($results) == 0 ? null : $results);
+}
