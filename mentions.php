51231debca9e2ab415004cbf972b73e52734719f
Sofiane SIFAOUI <sofiane.sifaoui1@gmail.com>
2023-05-01 14:52:13 +0200

    work on the responsive of mentions.php

--- mentions.php
+++ mentions.php
@@ -5,7 +5,6 @@
   <meta charset="utf-8" />
   <link rel="stylesheet" href="/CSS/styles.css" />
   <link rel="stylesheet" href="/CSS/mentions.css" />
-  <link rel="stylesheet" href="/CSS/presentation.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="icon" href="/images/logo-notext.png" type="image/icon type" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
