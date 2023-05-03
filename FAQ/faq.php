b8c57387975f99b8227cf7a95f91cf36255e89fd
Sofiane SIFAOUI <sofiane.sifaoui1@gmail.com>
2023-05-03 09:22:51 +0200

    add no response possibility for faq

--- FAQ/faq.php
+++ FAQ/faq.php
@@ -4,12 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>F.A.Q</title>
-  <link rel="stylesheet" href="./faq.css">
+  <link rel="stylesheet" href="faq.css">
   <link rel="icon" href="/images/logo-notext.png" type="image/icon type" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <script src="/JS/scripts.js"></script>
-  <link rel="stylesheet" href="https://kit.fontawesome.com/bc424452bc.css" crossorigin="anonymous" />
   <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Nunito&display=swap" rel="stylesheet" />
 
@@ -134,6 +133,7 @@
     </button>
   </div>
 </div>
+<h3 class="noResponse" style="color: #3f2de1; padding: 10px 20px;">Vous n'avez pas trouvé de réponse à votre question ? <a href="../contact/contact.php" style="text-decoration:none; color:#e0584c;">Contactez-nous !</a></h3>
 <!-- partial -->
   <script  src="./faq.js"></script>
 
