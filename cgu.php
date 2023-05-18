fbc95a35c932689171266f20af119d393567df54
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-05-18 17:06:58 +0200

    begin mergin cgu, mentions and FAQ

---
+++ cgu.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+
+<head>
+  <title>CGU</title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="/CSS/styles.css"/>
+  <link rel="stylesheet" href="/CSS/cgu.css"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" href="/images/logo-notext.png" type="image/icon type" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <script src="/JS/scripts.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Nunito&display=swap" rel="stylesheet" />
+</head>
+
+<html>
+    <body>
+        <?php require "top-bar.php"; ?>
+
+        <div class="wrapper">
+            <div class="box">
+            <h3 id="title">Conditions générales d'utilisation (CGU)</h3>
+            <div class="corps">
+            <table>
+            <tr>
+                <p class="textcorp">
+                    CONDITIONS GÉNÉRALES D'UTILISATION DU DISPOSITIF HEALTH-EIR
+                    <br><br>
+                    Le présent document énonce les conditions générales d'utilisation du dispositif Health-Eir. En utilisant ce dispositif, vous acceptez les conditions générales suivantes :
+                    <br><br>
+                    <span>1. Objet</span>
+                    <br>
+                    Le dispositif Health-Eir est un boîtier de monitoring de la qualité de prise en charge d'un patient hospitalisé à domicile. Il permet de mesurer des grandeurs environnementales (température, taux de microparticules dans la pièce, taux de CO2) et des données physiologiques comme la fréquence cardiaque. Les données sont consultables en temps réel par la famille du patient ainsi que par le médecin traitant qui peut paramétrer les seuils des capteurs.
+                    <br><br>
+                    <br><br>
+                    <span>2. Utilisation du dispositif</span>
+                    <br>
+                    L'utilisation du dispositif Health-Eir est réservée aux patients hospitalisés à domicile ainsi qu'aux membres de leur famille et aux professionnels de santé impliqués dans leur prise en charge. Les utilisateurs s'engagent à utiliser le dispositif de manière responsable et à respecter les règles de confidentialité des données.
+                    <br><br>
+                    <br><br>
+                    <span>3. Données collectées</span>
+                    <br>
+                    Les données collectées par le dispositif Health-Eir sont traitées de manière confidentielle et conformément aux lois en vigueur sur la protection des données. Les données sont stockées sur des serveurs sécurisés et ne sont accessibles qu'aux utilisateurs autorisés.
+                    <br><br>
+                    <br><br>
+                    <span>4. Responsabilités</span>
+                    <br>
+                    Le fabricant du dispositif Health-Eir décline toute responsabilité en cas d'utilisation non conforme du dispositif ou de perte ou de dommage résultant de l'utilisation du dispositif. Les utilisateurs sont responsables de l'utilisation qu'ils font du dispositif et doivent se conformer aux lois en vigueur sur la protection des données et sur la santé.
+                    <br><br>
+                    <br><br>
+                    <span>5. Durée du contrat</span>
+                    <br>
+                    Le contrat entre le fabricant du dispositif Health-Eir et l'utilisateur est conclu pour une durée indéterminée. Les utilisateurs peuvent mettre fin au contrat à tout moment en cessant d'utiliser le dispositif.
+                    <br><br>
+                    <br><br>
+                    <span>6. Modification des conditions d'utilisation</span>
+                    <br>
+                    Le fabricant du dispositif Health-Eir se réserve le droit de modifier les présentes conditions d'utilisation à tout moment. Les utilisateurs seront informés des modifications par e-mail ou par une notification sur le site web. Les modifications prendront effet à compter de leur publication sur le site web.
+                    <br><br>
+                    En utilisant le dispositif Health-Eir, vous acceptez les présentes conditions générales d'utilisation. Si vous ne les acceptez pas, veuillez cesser d'utiliser le dispositif.
+                </p>
+                <p class="textcorp">
+                </p>
+                </td>
+            </tr>
+            </table>
+      </div>
+
+    </body>
+</html>
