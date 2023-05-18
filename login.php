2b921044cfa9530dfb8eba0acb4581bbd8434d3b
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-05-18 22:54:31 +0200

    new form on register also done

--- login.php
+++ login.php
@@ -19,6 +19,8 @@ if (isset($_SESSION["name"])) {
   <meta charset="utf-8" />
   <title id="connexion">Login</title>
   <script src="/JS/scripts.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href="https://fonts.googleapis.com/css?family=Krona+One" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Reem+Kufi" rel="stylesheet" />
@@ -27,71 +29,88 @@ if (isset($_SESSION["name"])) {
 <body>
   <?php require "top-bar.php"; ?>
   <div class="wrapper">
-    <div class="login">
-      <h1 class="title_connexion">CONNEXION</h1>
-      <form action="" method="POST" class="form_log">
-        <input class="email" type="email" name="email" placeholder="Email:" />
+    <form action="" method="POST">
+      <h1>CONNEXION</h1>
+      <div class="separation"></div>
+      <div class="corps-formulaire">
+        <div class="gauche">
+          <div class="groupe">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Email:" />
+            <i class="fa-solid fa-envelope"></i>
+          </div>
+          <div class="groupe">
+            <label>Mot de passe</label>
+            <input type="password" name="password" placeholder="Mot de passe:" />
+            <i class="fa-solid fa-key"></i>
+          </div>
 
-        <input class="mdp" type="password" name="password" placeholder="Mot de passe:" />
+          <a class="link" href="">Mot de passe oublié ?</a>
+          <div class="remember">
+            <input type="checkbox" id="rememberme" name="check" checked />
+            <label for="check">Se souvenir de moi</label>
+          </div>
+          <div class="pied-formulaire">
+            <button id="btn" type="submit">Se connecter</button>
+          </div>
+          <div id="loader" class="loader"></div>
+          <script>
+            const btn = document.getElementById("btn");
+            const loader = document.getElementById("loader");
 
-        <a href="">Mot de passe oublié ?</a>
-        <div class="remember">
-          <input type="checkbox" id="rememberme" name="check" checked />
-          <label for="check">Se souvenir de moi</label>
-        </div>
+            btn.addEventListener("click", () => {
+              loader.style.display = "block";
+              // Code pour lancer une requête ou une opération qui prend du temps
+              setTimeout(() => {
+                loader.style.display = "none";
+              }, 2000); // Temps en millisecondes avant de cacher le loader
+            });
+          </script>
 
-        <button id="btn" type="submit" class="connect_button">Se connecter</button>
-      </form>
-      <div id="loader" class="loader"></div>
-      <script>
-        const btn = document.getElementById("btn");
-        const loader = document.getElementById("loader");
-
-        btn.addEventListener("click", () => {
-          loader.style.display = "block";
-          // Code pour lancer une requête ou une opération qui prend du temps
-          setTimeout(() => {
-            loader.style.display = "none";
-          }, 2000); // Temps en millisecondes avant de cacher le loader
-        });
-      </script>
-
-      <?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        require "database.php";
-        // Get the email and password from the form
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-        $stmt = $_DB->execute(
-          "SELECT password, surname, name, roles.role_name, roles.role_permission FROM users JOIN roles on users.id_role = roles.id_role WHERE email = :email",
-          ["email" => $email]
-        );
-        if ($stmt->rowCount() > 0) {
-          $row = $stmt->fetch();
-          $password_hash = $row["password"];
-          if (password_verify($password, $password_hash)) {
-            echo "Password is valid!";
-            session_start();
-            $_SESSION["name"] = $row["name"];
-            $_SESSION["surname"] = $row["surname"];
-            $_SESSION["email"] = $email;
-            $_SESSION["role_name"] = $row["role_name"];
-            $_SESSION["role_permission"] = $row["role_permission"];
-            if ($_SESSION["role_permission"] == 6) {
-              header("Location: /admin/admin.php");
+          <?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            require "database.php";
+            // Get the email and password from the form
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $stmt = $_DB->execute(
+              "SELECT password, surname, name, roles.role_name, roles.role_permission FROM users JOIN roles on users.id_role = roles.id_role WHERE email = :email",
+              ["email" => $email]
+            );
+            if ($stmt->rowCount() > 0) {
+              $row = $stmt->fetch();
+              $password_hash = $row["password"];
+              if (password_verify($password, $password_hash)) {
+                echo "Password is valid!";
+                session_start();
+                $_SESSION["name"] = $row["name"];
+                $_SESSION["surname"] = $row["surname"];
+                $_SESSION["email"] = $email;
+                $_SESSION["role_name"] = $row["role_name"];
+                $_SESSION["role_permission"] = $row["role_permission"];
+                if ($_SESSION["role_permission"] == 6) {
+                  header("Location: /admin/admin.php");
+                } else {
+                  header("Location: /");
+                }
+              } else {
+                echo '<p style="color: red;">Error, invalid password !</p>';
+              }
             } else {
-              header("Location: /");
+              echo "Error, bad credentials";
             }
-          } else {
-            echo '<p style="color: red;">Error, invalid password !</p>';
-          }
-        } else {
-          echo "Error, bad credentials";
-        }
-      } ?>
+          } ?>
 
-      <a class="link_compt_crea" href="/register.php">Vous n'avez pas de compte ? Créez le</a>
-    </div>
+        </div>
+        <div class="separation" style="margin-top: 20px;margin-bottom: 20px;"></div>
+        <div class="droite">
+          <a>Je n'ai pas encore de compte</a>
+          <a class="link" href="/register.php">M'inscrire</a>
+        </div>
+      </div>
+    </form>
+
   </div>
-<?php require "bottom-bar.php"; ?>
+  <?php require "bottom-bar.php"; ?>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -14,6 +14,12 @@ if (isset($_SESSION["name"])) {
   <link rel="icon" href="/images/logo-notext.png" type="image/icon type" />
   <link href="https://fonts.googleapis.com/css?family=Krona+One" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title>Register</title>
   <script src="/JS/scripts.js"></script>
 </head>
@@ -21,150 +27,201 @@ if (isset($_SESSION["name"])) {
 <body>
   <?php require "top-bar.php"; ?>
   <div class="wrapper">
-    <div class="inscription">
-      <h1 class="title_register">INSCRIPTION</h1>
-      <form method="POST" class="form_inscription">
-        <select name="role" class="type">
-          <option value=select>Choisissez votre statut</option>
-          <option value=famille>Patient/Famille</option>
-          <option value=medecin>Medecin</option>
-        </select>
+    <form action="" method="POST">
+      <h1>INSCRIPTION</h1>
+      <div class="separation"></div>
+      <div class="corps-formulaire">
 
-        <div class="gender">
-          <div class="h">
-            <input type="checkbox" id="h" name="gender" value="M" checked>
-            <label for="h">Homme</label>
+        <div class="gauche">
+
+          <select name="role" class="type">
+            <option value=select>Choisissez votre statut</option>
+            <option value=famille>Patient/Famille</option>
+            <option value=medecin>Medecin</option>
+          </select>
+
+          <div class="gender">
+            <div class="h">
+              <input type="checkbox" id="h" name="gender" value="M" checked>
+              <label for="h">Homme</label>
+            </div>
+
+            <div class="f">
+              <input type="checkbox" id="f" name="gender" value="F">
+              <label for="f">Femme</label>
+            </div>
           </div>
 
-          <div class="f">
-            <input type="checkbox" id="f" name="gender" value="F">
-            <label for="f">Femme</label>
-          </div>
-        </div>
+          <script>
+            const checkboxes = document.querySelectorAll('input[name="gender"]');
 
-        <script>
-          const checkboxes = document.querySelectorAll('input[name="gender"]');
-
-          checkboxes.forEach((checkbox) => {
-            checkbox.addEventListener("change", (event) => {
-              checkboxes.forEach((cb) => {
-                if (cb !== event.target) {
-                  cb.checked = false;
-                }
+            checkboxes.forEach((checkbox) => {
+              checkbox.addEventListener("change", (event) => {
+                checkboxes.forEach((cb) => {
+                  if (cb !== event.target) {
+                    cb.checked = false;
+                  }
+                });
               });
             });
-          });
-        </script>
+          </script>
 
-        <input class="Nom" type="text" name="name" placeholder="Prenom:" required>
-        <input class="Prénom" type="text" name="surname" placeholder="Nom:" required>
-        <input class="email" type="email" name="email" placeholder="Email:" required>
-        <input id="password" class="mdp" type="password" placeholder="Mot de passe:" required>
-        <input id="confirm-password" class="mdp" type="password" name="password" placeholder="Confirmation mot de passe:" required>
-        <div id="password-match-message" style="display:none;color:red;">Passwords doesn't match</div>
-        <button id="submit-button" type="submit" class="register_button">S'inscrire</button>
-        <script>
-          const passwordInput = document.getElementById("password");
-          const confirmPasswordInput = document.getElementById("confirm-password");
-          const submitButton = document.getElementById("submit-button")
-          const message = document.getElementById("password-match-message");
+          <div class="groupe">
+            <label>Prénom</label>
+            <input type="text" name="name" required>
+            <i class="fa-solid fa-user"></i>
+          </div>
 
-          function checkPasswordMatch() {
-            if (passwordInput.value !== confirmPasswordInput.value) {
-              submitButton.disabled = true;
-              submitButton.style.pointerEvents = 'none';
-              submitButton.style.opacity = '0.5';
-              message.style.display = "block";
-            } else {
-              submitButton.disabled = false;
-              submitButton.style.pointerEvents = 'auto';
-              submitButton.style.opacity = '1';
-              message.style.display = "none";
-            }
-          }
+          <div class="groupe">
+            <label>Nom</label>
+            <input type="text" name="surname" required>
+            <i class="fa-solid fa-user"></i>
+          </div>
 
-          passwordInput.addEventListener("input", checkPasswordMatch);
-          confirmPasswordInput.addEventListener("input", checkPasswordMatch);
-        </script>
+          <div class="groupe">
+            <label>Email</label>
+            <input type="email" name="email" />
+            <i class="fa-solid fa-envelope"></i>
+          </div>
 
-      </form>
-      <?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        require "database.php";
-        $role = null;
-        switch ($_POST["role"]) {
-          case "famille":
-            $role = 2;
-            break;
-          case "medecin":
-            $role = 3;
-            break;
-          default:
-            $role = 2;
-            break;
-        }
-        $gender = $_POST["gender"];
-        $name = htmlspecialchars($_POST["name"]);
-        $surname = htmlspecialchars($_POST["surname"]);
-        $email = htmlspecialchars($_POST["email"]);
-        $id = md5($email);
-        $password = $_POST["password"];
+          <div class="groupe">
+            <label>Mot de passe</label>
+            <input id="password" type="password" name="password" />
+            <i class="fa-solid fa-key"></i>
+          </div>
 
-        $stmt = $_DB->execute("SELECT * FROM users WHERE id_user = :id", [
+          <div class="groupe">
+            <label>Confirmation du mot de passe</label>
+            <input id="confirm-password" type="password" name="password" required>
+            <i class="fa-solid fa-key"></i>
+          </div>
+
+          <div id="password-match-message" style="margin-top:20px;display:none;color:red;">Passwords doesn't match</div>
+          <div class="pied-formulaire">
+            <button id="submit-btn" type="submit">S'inscrire'</button>
+            <script>
+              const passwordInput = document.getElementById("password");
+              const confirmPasswordInput = document.getElementById("confirm-password");
+              const submitButton = document.getElementById("submit-btn")
+              const message = document.getElementById("password-match-message");
+
+              function checkPasswordMatch() {
+                if (passwordInput.value !== confirmPasswordInput.value) {
+                  console.log(passwordInput.value)
+                  submitButton.disabled = true;
+                  submitButton.style.pointerEvents = 'none';
+                  submitButton.style.opacity = '0.5';
+                  message.style.display = "block";
+                } else {
+                  submitButton.disabled = false;
+                  submitButton.style.pointerEvents = 'auto';
+                  submitButton.style.opacity = '1';
+                  message.style.display = "none";
+                }
+              }
+
+              passwordInput.addEventListener("input", checkPasswordMatch);
+              confirmPasswordInput.addEventListener("input", checkPasswordMatch);
+            </script>
+          </div>
+          <div id="loader" class="loader"></div>
+          <script>
+            const btn = document.getElementById("submit-btn");
+            const loader = document.getElementById("loader");
+
+            btn.addEventListener("click", () => {
+              loader.style.display = "block";
+              // Code pour lancer une requête ou une opération qui prend du temps
+              setTimeout(() => {
+                loader.style.display = "none";
+              }, 2000); // Temps en millisecondes avant de cacher le loader
+            });
+          </script>
+
+        </div>
+        <div class="separation" style="margin-top: 20px;margin-bottom: 20px;"></div>
+        <div class="droite">
+          <a>J'ai déjà un compte</a>
+          <a class="link" href="/login.php">Me connecter</a>
+        </div>
+      </div>
+    <?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      require "database.php";
+      $role = null;
+      switch ($_POST["role"]) {
+        case "famille":
+          $role = 3;
+          break;
+        case "medecin":
+          $role = 2;
+          break;
+        default:
+          $role = 3;
+          break;
+      }
+      $gender = $_POST["gender"];
+      $name = htmlspecialchars($_POST["name"]);
+      $surname = htmlspecialchars($_POST["surname"]);
+      $email = htmlspecialchars($_POST["email"]);
+      $id = md5($email);
+      $password = $_POST["password"];
+
+      $stmt = $_DB->execute("SELECT * FROM users WHERE id_user = :id", [
+        "id" => $id,
+      ]);
+      if ($stmt->rowCount() > 0) {
+        echo '<p style="color: red;text-align: center;">Error, user with this email already exist !</p>';
+      } else {
+        // Generate hashed password using PASSWORD_ARGON2ID algorithm
+        $options = [
+          "memory_cost" => PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+          "time_cost" => PASSWORD_ARGON2_DEFAULT_TIME_COST,
+          "threads" => PASSWORD_ARGON2_DEFAULT_THREADS,
+        ];
+        $hashedPassword = password_hash(
+          $password,
+          PASSWORD_ARGON2ID,
+          $options
+        );
+
+        // Prepare and execute the query to insert the new user into the database
+        $sql =
+          "INSERT INTO users (id_user, name, surname, email, password, id_role, gender) VALUES (:id,:name, :surname, :email, :password, :role, :gender)";
+        $stmt = $_DB->execute($sql, [
           "id" => $id,
+          "name" => $name,
+          "surname" => $surname,
+          "email" => $email,
+          "password" => $hashedPassword,
+          "role" => $role,
+          "gender" => $gender,
         ]);
+
+        // Check if the insert was successful
         if ($stmt->rowCount() > 0) {
-          echo '<p style="color: red;">Error, user with this email already exist !</p>';
+          $_SESSION["name"] = $name;
+          $_SESSION["surname"] = $surname;
+          $_SESSION["email"] = $email;
+          $row = $_DB
+            ->execute(
+              "SELECT role_name,role_permission from roles where id_role = :id",
+              [
+                "id" => $role,
+              ]
+            )
+            ->fetch();
+          $_SESSION["role_name"] = $row["role_name"];
+          $_SESSION["role_permission"] = $row["role_permission"];
+          echo '<script>window.location.href = "/";</script>';
         } else {
-          // Generate hashed password using PASSWORD_ARGON2ID algorithm
-          $options = [
-            "memory_cost" => PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
-            "time_cost" => PASSWORD_ARGON2_DEFAULT_TIME_COST,
-            "threads" => PASSWORD_ARGON2_DEFAULT_THREADS,
-          ];
-          $hashedPassword = password_hash(
-            $password,
-            PASSWORD_ARGON2ID,
-            $options
-          );
-
-          // Prepare and execute the query to insert the new user into the database
-          $sql =
-            "INSERT INTO users (id_user, name, surname, email, password, id_role, gender) VALUES (:id,:name, :surname, :email, :password, :role, :gender)";
-          $stmt = $_DB->execute($sql, [
-            "id" => $id,
-            "name" => $name,
-            "surname" => $surname,
-            "email" => $email,
-            "password" => $hashedPassword,
-            "role" => $role,
-            "gender" => $gender,
-          ]);
-
-          // Check if the insert was successful
-          if ($stmt->rowCount() > 0) {
-            $_SESSION["name"] = $name;
-            $_SESSION["surname"] = $surname;
-            $_SESSION["email"] = $email;
-            $row = $_DB
-              ->execute(
-                "SELECT role_name,role_permission from roles where id_role = :id",
-                [
-                  "id" => $role,
-                ]
-              )
-              ->fetch();
-            $_SESSION["role_name"] = $row["role_name"];
-            $_SESSION["role_permission"] = $row["role_permission"];
-            echo '<script>window.location.href = "/";</script>';
-          } else {
-            echo '<p style="color: red;">Error creating user !</p>';
-          }
+          echo '<p style="color: red;text-align: center;">Error creating user !</p>';
         }
-      } ?>
+      }
+    } ?>
 
-    </div>
+    </form>
   </div>
-<?php require "bottom-bar.php"; ?>
+  <?php require "bottom-bar.php"; ?>
 </body>
 
 </html>
