9f10fe34aa6b41929012fdab90dc7898ccb56c3a
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-06-03 15:44:17 +0200

    fixed footer + minor bugs on phone view

--- views/bottom-bar.php
+++ views/bottom-bar.php
@@ -1,26 +1,33 @@
+<!DOCTYPE html>
+<body>
+<head>
+    <script src="https://kit.fontawesome.com/bc424452bc.js" crossorigin="anonymous"></script>
+</head>
 <div class="bottom-bar">
-    <div class="left-items">
+    <div class="row1">
         <div class="links">
-            <a href="/index.php/mentions">&#9737; © TAKE-EIR</a>
-            <a href="/index.php/cgu">&#9737; CGU </a>
-            <a href="/index.php/faq">&#9737; FAQ </a>
-            <a href="/index.php/contact">&#9737; Nous contacter</a>
+            <a class="title">© TAKE-EIR</a>
+            <a href="/index.php/mentions">Mentions légales</a>
+            <a href="/index.php/cgu">CGU </a>
+            <a href="/index.php/faq">FAQ </a>
         </div>
         <div class="links">
-            <a href="/index.php/mentions">&#9737; © TAKE-EIR</a>
-            <a href="/index.php/cgu">&#9737; CGU </a>
-            <a href="/index.php/faq">&#9737; FAQ </a>
-            <a href="/index.php/contact">&#9737; Nous contacter</a>
+            <a class="title">Plan du site</a>
+            <a href="/index.php/presentation">Notre équipe</a>
+            <a href="/index.php/produit">Notre produit</a>
+            <a href="/index.php/contact">Nous contacter</a>
         </div>
+    </div>
+    <div class="row2">
+        Ce site a été réalisé avec <i class="fa fa-heart"></i> par les étudiants de l'ISEP, </br>
+        Institut Supérieur du Numérique, 10 rue de Vanves, 92130 Issy-les-Moulineaux
     </div>
     <div class="medias">
-        <!-- https://icons8.com/icons/set/social-media -->
-        <a href="https://linkedin.com/"><img src="/images/icons8-linkedin-24.png" />
-        </a>
-        <a href="https://youtube.com/"><img src="/images/icons8-youtube-logo-24.png" /></a>
-        <a href="https://twitter.com/Take_Eir"><img src="/images/icons8-twitter-24.png" />
-        </a>
-        <a href="https://instagram.com/take_eir"><img src="/images/icons8-instagram-24.png" />
-        </a>
+        <a href="https://linkedin.com/"><i class="fa fa-linkedin-square"></i></a>
+        <a href="https://youtube.com/"><i class="fa fa-youtube-square"></i></a>
+        <a href="https://twitter.com/Take_Eir"><i class="fa fa-twitter-square"></i></a>
+        <a href="https://instagram.com/take_eir"><i class="fa fa-instagram"></i></a>
     </div>
 </div>
+</body>
+</html>
--- views/login.php
+++ views/login.php
@@ -21,7 +21,6 @@ if (isset($_SESSION["name"])) {
   <script src="/JS/scripts.js"></script>
   <script src="/JS/users.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href="https://fonts.googleapis.com/css?family=Krona+One" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Reem+Kufi" rel="stylesheet" />
