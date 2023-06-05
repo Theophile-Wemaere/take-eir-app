1c8b18e51f09bb79737bfc6b9677eaebf754c83c
Sofiane SIFAOUI <sofiane.sifaoui1@gmail.com>
2023-06-05 18:28:43 +0200

    new carroussel 3D

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
     <script src="/JS/scripts.js"></script>
     <link rel="stylesheet" href="https://kit.fontawesome.com/bc424452bc.css" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Nunito&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Nunito&display=swap"
+        rel="stylesheet" />
 </head>
 
 <html>
@@ -39,13 +40,13 @@
             <div class="carousel">
                 <div class="carousel__face"><span>Health-Eir</span></div>
                 <div class="carousel__face"><span>Révolutionne</span></div>
-                <div class="carousel__face"><span>Le concept</span></div>
-                <div class="carousel__face"><span>De l'hospitalisation à domicile</span></div>
+                <div class="carousel__face"><span class="v2">Le concept</span></div>
+                <div class="carousel__face"><span>d'hospitalisation à domicile</span></div>
                 <div class="carousel__face"><span>Notre équipe</span></div>
                 <div class="carousel__face"><span>a travaillé dur</span></div>
-                <div class="carousel__face"><span>pour vous proposer</span></div>
+                <div class="carousel__face"><span class="v2">pour vous proposer</span></div>
                 <div class="carousel__face"><span>ce produit d'exception</span></div>
-                <div class="carousel__face"><span>Essayez-le !</span></div>
+                <div class="carousel__face"><span class="take-eir">Essayez-le !</span></div>
             </div>
         </div>
     </div>
