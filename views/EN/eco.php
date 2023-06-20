22a347f517c19e318081a162bf9aa265d4e176ea
Imad-R <92092028+Imad-R@users.noreply.github.com>
2023-06-20 10:12:37 +0200

    eco ppage push 3

--- views/EN/eco.php
+++ views/EN/eco.php
@@ -1,87 +1,111 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<title>Engagement écologique</title>
+    <title>Ecological Commitment</title>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="/take-eir-app/CSS/styles.css" />
-    <link rel="stylesheet" href="/take-eir-app/CSS/cgu.css" />
+    <link rel="stylesheet" href="/take-eir-app/CSS/eco.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/images/logo-notext.png" type="image/icon type" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <script src="/JS/scripts.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Nunito&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Nunito&display=swap"
+        rel="stylesheet" />
 </head>
+
 <body>
     <?php require "top-bar.php"; ?>
 
     <div class="wrapper">
         <div class="box">
-            <h3 id="title">Engagement environnemental</h3>
+            <h3 id="title">Environmental Commitment</h3>
             <div class="corps">
                 <table>
                     <tr>
                         <img class="ecoPicture" src="/take-eir-app/images/ecologique.png">
                         <p class="textcorp">
-                            
-                            Chez Take EIR, nous accordons une importance primordiale à l'éco-conception de nos produits. Nous nous engageons à développer 
-                            des solutions numériques en santé qui non seulement améliorent la vie des patients, mais qui sont également respectueuses 
-                            de l'environnement. Dans cet esprit, notre projet "Health-EIR" intègre des mesures concrètes pour minimiser notre empreinte 
-                            écologique tout au long du cycle de vie du produit, depuis sa conception jusqu'à sa fin de vie.
+
+                            At Take EIR, we place utmost importance on the eco-design of our products. We are committed to
+                            developing digital health solutions that not only improve patients' lives but also
+                            respect the environment. In line with this, our project "Health-EIR" incorporates concrete
+                            measures to minimize our ecological footprint throughout the product lifecycle, from design
+                            to end-of-life.
                             <br><br>
                             <br><br>
-                            <span style="margin-right: 4%;">Voici quelques-unes des mesures que nous avons prises :</span>
+                            <h2>Here are some of the measures we have taken:</h2>
                             <br><br>
                             <br><br>
-                            <span class="ecoIssue">Conception éco-responsable :</span>
-                            <br><br>
-                            Lors de la conception du boîtier électronique Health-EIR, nous avons intégré les principes de l'éco-conception afin de 
-                            minimiser son impact sur l'environnement. Notre boîtier a été conçu en utilisant principalement du PLA, qui est le matériau 
-                            consommable le plus couramment utilisé dans l'impression 3D FDM à dépôt de fil. Le PLA est un plastique écologique 
-                            d'origine végétale, fabriqué à partir d'amidon de maïs, de racines de manioc et de betterave. Il est biodégradable, 
-                            non toxique et dégage une légère odeur sucrée pendant le processus d'impression.
-                            <br><br>
-                            <br><br>
-                            <span class="ecoIssue">Optimisation de la consommation d'énergie :</span>
-                            <br><br>
-                            Nous avons intégré des technologies efficaces sur le plan énergétique afin de réduire la consommation d'électricité 
-                            du boitier. Cela permet non seulement de limiter notre empreinte carbone, mais également de prolonger la durée de vie 
-                            de la batterie.
-                            <br><br>
-                            <br><br>
-                            <span class="ecoIssue">Réparabilité et modularité :</span>
-                            <br><br>
-                            Nous concevons le boitier Health-EIR de manière à faciliter sa réparation et son évolutivité. Ainsi, en cas de panne 
-                            ou d'obsolescence d'une partie du dispositif, nous encourageons la réparation ou le remplacement uniquement de la 
-                            composante défectueuse, ce qui réduit le gaspillage de ressources.
-                            <br><br>
-                            <br><br>
-                            <span class="ecoIssue">Objectifs environnementaux :</span>
-                            <br><br>
-                            En tant que startup engagée en faveur de la durabilité, nous nous fixons des objectifs environnementaux ambitieux 
-                            pour minimiser notre impact sur la planète.
-                            <br><br>
-                            <br><br>
-                            <span class="ecoIssue">Réduction des émissions de gaz à effet de serre :</span>
-                            <br><br>
-                            Nous cherchons à optimiser l'efficacité énergétique du boitier Health-EIR pour réduire les émissions de CO2 associées 
-                            à son utilisation. Nous nous engageons également à compenser nos émissions résiduelles en soutenant des projets de 
-                            séquestration du carbone ou en investissant dans des initiatives de développement durable.
-                            <br><br>
-                            <br><br>
-                            <span class="ecoIssue">Gestion responsable des déchets :</span>
-                            <br><br>
-                            Nous encourageons activement le recyclage des composants électroniques en fin de vie du boitier Health-EIR. 
-                            Nous travaillons en collaboration avec des partenaires spécialisés dans le recyclage électronique pour assurer 
-                            une élimination responsable et respectueuse de l'environnement des déchets électroniques.
-                            <br><br>
-                            <br><br>
-                            <span class="ecoIssue">Sensibilisation à l'éco-responsabilité :</span>
-                            <br><br>
-                            Nous nous engageons à sensibiliser nos clients, nos employés et nos partenaires sur l'importance de l'éco-responsabilité. 
-                            Nous partageons régulièrement des informations sur les bonnes pratiques environnementales et nous encourageons l'adoption 
-                            de comportements durables.
+                        <div class="titleEco">
+                            <span class="ecoIssue">Eco-responsible design:</span>
+                            <img class="ecoPicture_2" src="/take-eir-app/images/recycler.png">
+                        </div>
+                        <br><br>
+                        When designing the Health-EIR electronic casing, we integrated eco-design principles to
+                        minimize its environmental impact. Our casing was primarily made using PLA, which is the most
+                        commonly used consumable material in FDM 3D printing. PLA is an eco-friendly bioplastic made
+                        from corn starch, cassava roots, and beetroot. It is biodegradable, non-toxic, and emits a
+                        slight sweet odor during the printing process.
+                        <br><br>
+                        <br><br>
+                        <div class="titleEco">
+                            <span class="ecoIssue">Energy consumption optimization:</span>
+                            <img class="ecoPicture_2" src="/take-eir-app/images/energie-ecologique.png">
+                        </div>
+                        <br><br>
+                        We have incorporated energy-efficient technologies to reduce the power consumption of the
+                        casing. This not only helps limit our carbon footprint but also extends the battery life.
+                        <br><br>
+                        <br><br>
+                        <div class="titleEco">
+                            <span class="ecoIssue">Repairability and modularity:</span>
+                            <img class="ecoPicture_2" src="/take-eir-app/images/sac-plastique.png">
+                        </div>
+                        <br><br>
+                        We design the Health-EIR casing to facilitate repair and upgradability. Thus, in case of a
+                        malfunction or obsolescence of a device component, we encourage the repair or replacement of
+                        only the defective part, reducing resource waste.
+                        <br><br>
+                        <br><br>
+                        <div class="titleEco">
+                            <span class="ecoIssue">Environmental goals:</span>
+                            <img class="ecoPicture_2" src="/take-eir-app/images/respectueux-de-la-nature.png">
+                        </div>
+                        <br><br>
+                        As a sustainability-driven startup, we set ambitious environmental goals to minimize our impact
+                        on the planet.
+                        <br><br>
+                        <br><br>
+                        <div class="titleEco">
+                            <span class="ecoIssue">Reduction of greenhouse gas emissions:</span>
+                            <img class="ecoPicture_2" src="/take-eir-app/images/co2_2.png">
+                        </div>
+                        <br><br>
+                        We strive to optimize the energy efficiency of the Health-EIR casing to reduce the CO2 emissions
+                        associated with its use. We are also committed to offsetting our residual emissions by
+                        supporting carbon sequestration projects or investing in sustainable development initiatives.
+                        <br><br>
+                        <br><br>
+                        <div class="titleEco">
+                            <span class="ecoIssue">Responsible waste management:</span>
+                            <img class="ecoPicture_2" src="/take-eir-app/images/zero-pour-cent.png">
+                        </div>
+                        <br><br>
+                        We actively encourage recycling of end-of-life electronic components from the Health-EIR
+                        casing. We collaborate with specialized partners in electronic recycling to ensure responsible
+                        and environmentally friendly disposal of electronic waste.
+                        <br><br>
+                        <br><br>
+                        <div class="titleEco">
+                            <span class="ecoIssue">Promoting eco-responsibility:</span>
+                            <img class="ecoPicture_2" src="/take-eir-app/images/empreinte-carbone.png">
+                        </div>
+                        <br><br>
+                        We are committed to raising awareness among our customers, employees, and partners about the
+                        importance of eco-responsibility. We regularly share information on good environmental practices
+                        and encourage the adoption of sustainable behaviors.
                         </p>
                         <img class="ecoPicture" src="/take-eir-app/images/ecologique_2.png">
                         <p class="textcorp">
